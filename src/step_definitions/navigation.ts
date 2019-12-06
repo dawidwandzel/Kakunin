5c53378accce9be6122412e86d673b5da6240106
DawidWandzel <dawid.wandzel@thesoftwarehouse.pl>
2019-12-06 12:41:48 +0100

    Added step to handle basic auth. Added functional tests.

--- src/step_definitions/navigation.ts
+++ src/step_definitions/navigation.ts
@@ -1,4 +1,6 @@
 import { Then, Given } from 'cucumber';
+import { isRelativePage } from '../web/url-parser.helper';
+import config from '../core/config.helper';
 
 Given(/^I visit the "([^"]*)" page$/, function(pageName) {
   expect(browser.page[pageName]).toBeDefined();
@@ -27,4 +29,12 @@ Then(/^the "([^"]*)" page is displayed$/, function(pageName) {
     self.currentPage = browser.page[pageName];
     self.urlParameters = checkResult.parameters;
   });
+});
+
+Given(/^I visit the "([^"]*)" page with "([^"]*)" basic auth credentials$/, function(pageName, credentials) {
+  expect(browser.page[pageName]).toBeDefined();
+  this.currentPage = browser.page[pageName];
+  const url = isRelativePage(this.currentPage.url) ? `${config.baseUrl}${this.currentPage.url}` : this.currentPage.url;
+  const splittedUrl = url.split('//');
+  return browser.get(`${splittedUrl[0]}//${credentials}@${splittedUrl[1]}`);
 });
